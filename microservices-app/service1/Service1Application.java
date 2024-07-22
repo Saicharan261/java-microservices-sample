5d062034c4ad76675cc291f1ff163ce1f2ee663a
Sai Charan <saicharan9008@gmail.com>
2024-07-22 16:53:55 +0530

    Initial commit

---
+++ microservices-app/service1/Service1Application.java
@@ -0,0 +1,21 @@
+package com.example.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Service1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Service1Application.class, args);
+    }
+}
+
+@RestController
+class Service1Controller {
+    @GetMapping("/service1")
+    public String service1() {
+        return "Hello from Service 1";
+    }
+}
